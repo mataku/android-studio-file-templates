eb7fc7e187211ab802301bd13e07905371986b9c
GitHub Action <action@github.com>
2022-07-19 02:19:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/mataku/androidstudiofiletemplates/MyBundle.kt
+++ src/main/kotlin/com/github/mataku/androidstudiofiletemplates/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mataku.androidstudiofiletemplates
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/mataku/androidstudiofiletemplates/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mataku.androidstudiofiletemplates.services
+
+import com.github.mataku.androidstudiofiletemplates.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
