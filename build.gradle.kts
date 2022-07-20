1394b5df5f5e756a6e22ec3f0db6a6909c97c79a
Takuma Homma <nagomimatcha@gmail.com>
2022-07-20 14:08:39 +0900

    Integrate kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,16 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:5.3.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.3.2")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+    maxHeapSize = "1024m"
+}
+
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
     pluginName.set(properties("pluginName"))
