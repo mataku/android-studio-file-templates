204fd63f604b3ef85b6783398fd1f68a5e389159
Takuma Homma <nagomimatcha@gmail.com>
2022-07-20 14:23:13 +0900

    Enable to choose whether to create JUnit or(/and) Kotest test files

--- src/main/kotlin/other/viewmodel/ViewModelRecipe.kt
+++ src/main/kotlin/other/viewmodel/ViewModelRecipe.kt
@@ -2,20 +2,49 @@ package other.viewmodel
 
 import com.android.tools.idea.wizard.template.ModuleTemplateData
 import com.android.tools.idea.wizard.template.RecipeExecutor
-import other.viewmodel.src.app_package.viewModelTemplate
+import other.viewmodel.src.app_package.viewModel
+import other.viewmodel.test.app_package.viewModelSpec
+import other.viewmodel.test.app_package.viewModelTest
+import util.slashedPackageName
+import java.io.File
 
 fun RecipeExecutor.viewModelRecipe(
     moduleData: ModuleTemplateData,
+    moduleName: String,
     packageName: String,
-    viewModelName: String
+    viewModelName: String,
+    junitTestFileRequired: Boolean,
+    kotestSpecFileRequired: Boolean
 ) {
     val (_, srcOut, _) = moduleData
 
+    val mainSourcePath = "${moduleName}/src/main/java/${packageName.slashedPackageName()}"
+    val testSourcePath = "${moduleName}/src/test/java/${packageName.slashedPackageName()}"
+
     save(
-        viewModelTemplate(
+        viewModel(
             packageName = packageName,
             viewModelName = viewModelName
         ),
-        srcOut.resolve("$viewModelName.kt")
+        File("$mainSourcePath/${viewModelName}.kt")
     )
+    if (junitTestFileRequired) {
+        save(
+            viewModelTest(
+                packageName = packageName,
+                viewModelName = viewModelName
+            ),
+            File("$testSourcePath/${viewModelName}Test.kt")
+        )
+    }
+    if (kotestSpecFileRequired) {
+        save(
+            viewModelSpec(
+                packageName = packageName,
+                viewModelName = viewModelName
+            ),
+            File("$testSourcePath/${viewModelName}Spec.kt")
+        )
+    }
 }
+
